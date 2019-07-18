f7970e25878e0fc95e512a05e5dca2318508e9c4
yrh6666 <52987311+yrh6666@users.noreply.github.com>
2019-07-18 18:03:53 +0800

    yrh

---
+++ MiniGame/Assets/Scripts/FlowchartMangerKeyplot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowchartMangerKeyplot : MonoBehaviour
+{
+    // Start is called before the first frame update
+struct datasaying
+    {
+        int numofplayer;
+    }
+}
